e7bb1aca208ec93b128eddd810d6396c00b0fa8f
Bastien Mourguiart <bmourgui@br157-127.ifremer.fr>
2023-08-11 14:59:01 +0200

    Edit file names

--- make.R
+++ make.R
@@ -1,17 +1,12 @@
 ######
 # Script of modelling workflow used for:
-# 'Time for addressing coarse-to-fine spatial misalignment in SDMs'
-# B. Mourguiart, M. Chevalier, M. Marzloff, N. Caill-Milly, K. Mengersen, B. Liquet
-#
-#
-
-
-
+# "Dealing with grain mismatch between response and explanatory variables in species distribution models."
+######
 
 # 1. Simulate data
 simulate <- TRUE
 if (simulate == TRUE){
-  source(here::here("analysis", "simu", "simu01_create_rasterize_occu_data.R"))
+  source(here::here("analysis", "01_simulate_data.R"))
 }else{
   load(here::here("results", "simu", "simulated_data.RData"))
 }
@@ -20,34 +15,37 @@ if (simulate == TRUE){
 ## 2.1. with bem
 run.bem <- TRUE
 if (run.bem == TRUE){
-  source(here::here("analysis", "simu", "simu03_nimble_bem.R"))
+  source(here::here("analysis", "02_run_bem.R")) # can be long
 }else{
-  #load(here::here("results", "simu", "output_bem_rep1a15.RData"))
+  load(here::here("results", "simu", "output_bem.RData"))
 }
 
 ## 2.2. with glm
 run.glm <- TRUE
 if (run.glm == TRUE){
-  source(here::here("analysis", "simu", "simu03_nimble_glm.R"))
+  source(here::here("analysis", "02_run_glm.R")) # can be long
 }else{
-  #load(here::here("results", "simu", "output_glm_rep1a15.RData"))
+  load(here::here("results", "simu", "output_glm.RData"))
 }
 
 ## 2.1. with spglm
 run.spglm <- TRUE
 if (run.spglm == TRUE){
-  source(here::here("analysis", "simu", "simu03_run_spglm.R"))
+  source(here::here("analysis", "02_run_spglm.R")) # can be long
 }else{
-  #load(here::here("results", "simu", "output_spglm+pred.RData"))
+  load(here::here("results", "simu", "output_spglm+pred.RData"))
 }
 
 # 3. Compute performance metrics
 run.perf <- TRUE
 if (run.perf == TRUE){
-  source(here::here("analysis", "simu", "simu04_compute_perfmetrics.R"))
+  source(here::here("analysis", "03_compute_perfmetrics.R")) # can be long
 }else{
   load(here::here("results", "simu", "summary_perf.RData"))
 }
 
-# 4. Produce results
-source(here::here("analysis", "simu", "simu05_analyze_perfmetrics.R"))
+# 4. Produce summary results
+source(here::here("analysis", "simu", "04_analyze_perfmetrics.R"))
+
+# 5. Plot results
+source(here::here("analysis", "simu", "05_make_outputs.R"))
